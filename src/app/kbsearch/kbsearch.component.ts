f2dcd019310ac1888811bc0b6b52cf195601fe33
srinivas padidapelli <spadidapelli@gmail.com>
2018-07-13 12:22:22 +0530

    clarity seed

---
+++ src/app/kbsearch/kbsearch.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-kbsearch',
+  templateUrl: './kbsearch.component.html',
+  styleUrls: ['./kbsearch.component.scss']
+})
+export class KbsearchComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
